9a37972a521e5c7aca0b5e1429be763cf05d2ba6
Jean Machuca <correojean@gmail.com>
2021-04-20 18:19:56 -0400

    codersrank widget

--- doc/js/cl.quickcorp.components.js
+++ doc/js/cl.quickcorp.components.js
@@ -9,5 +9,35 @@ Package("cl.quickcorp.components",[
     controller:null,
     view:null,
     templateHandler: "MarkdownTemplateHandler"
+  }),
+  Class('CustomMarkdownComponent',Component,{
+    dependencies:[],
+    body:document.createElement('div'),
+    name:'readmemarkdowncomponent',
+    tplsource:'external',
+    cached:true,
+    controller:null,
+    view:null,
+    templateHandler: 'MarkdownTemplateHandler',
+    done:function (){
+      _super_('Component','done').call(this);
+    }
+  }),
+  Class('ReadmeMarkdownComponent',CustomMarkdownComponent,{
+    cached:true,
+    name:"readmemarkdowncomponent",
+    templateURI:"./README.md"
+  }),
+  Class('AuthorMarkdownComponent',CustomMarkdownComponent,{
+    cached:true,
+    templateURI:"./AUTHOR.md"
+  }),
+  Class('ContributingMarkdownComponent',CustomMarkdownComponent,{
+    cached:true,
+    templateURI:"./CONTRIBUTING.md"
+  }),
+  Class('CoCMarkdownComponent',CustomMarkdownComponent,{
+    cached:true,
+    templateURI:"./CODE_OF_CONDUCT.md"
   })
 ]);
